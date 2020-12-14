f1cca5b7be84bc74f5e7e2a920422ad6659f72ab
hxd <llphxdgg@gmail.com>
2020-12-15 02:31:47 +0800

    创建剑法

---
+++ src/main/java/com/atguigu/Git02.java
@@ -0,0 +1,15 @@
+package com.atguigu;
+
+/**
+ * @author hxd  Email：llphxdgg@gmail.com
+ * @description
+ * @create 2020-12-15 2:19
+ */
+public class Git02 {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+    }
+}
